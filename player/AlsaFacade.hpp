aad040e382704e2e9e91c54147cc76fb20182b0b
Joachim Erbs <joachim.erbs@gmx.de>
2009-09-10 23:31:21 +0200

    Moved files into subdirectories

--- player/AlsaFacade.hpp
+++ player/AlsaFacade.hpp
@@ -1,28 +1,7 @@
-//
-// ALSA Interface
-//
-// Copyright (C) Joachim Erbs, 2009-2010
-//
-//    This file is part of Sinema.
-//
-//    Sinema is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    Sinema is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with Sinema.  If not, see <http://www.gnu.org/licenses/>.
-//
-
 #ifndef ALSA_FACADE_HPP
 #define ALSA_FACADE_HPP
 
-#include "player/GeneralEvents.hpp"
+#include "GeneralEvents.hpp"
 
 extern "C"
 {
@@ -30,11 +9,7 @@ extern "C"
 }
 
 #include <string>
-#include <boost/bind.hpp>
-#include <boost/function.hpp>
 #include <boost/shared_ptr.hpp>
-
-class CopyLog;
 
 class AFAudioFrame
 {
@@ -42,7 +17,7 @@ public:
     AFAudioFrame(int size)
 	: allocatedByteSize(size),
 	  frameByteSize(0),
-	  buf(new char[size]),        // 16 byte allignment needed here!
+	  buf(new char[size]),
 	  pts(0),
 	  offset(0)
     {}
@@ -61,7 +36,7 @@ public:
 	offset += bytes;
 	if (offset > frameByteSize)
 	{
-	    TRACE_ERROR(<<"Consumed too much data.");
+	    ERROR(<<"Consumed too much data.");
 	    exit(-1);
 	}
 	return &buf[off];
@@ -70,18 +45,10 @@ public:
     void setPTS(double pts_) {pts = pts_;}
     double getPTS() {return pts;}
 
-    void setNextPTS(double pts_) {nextPts = pts_;}
-    double getNextPTS() {return nextPts;}
-
     void reset()
     {
 	frameByteSize = 0;
 	pts = 0;
-	offset = 0;
-    }
-
-    void restore()
-    {
 	offset = 0;
     }
 
@@ -104,30 +71,17 @@ private:
     int frameByteSize;
     char* buf;
     double pts;
-    double nextPts;
     int offset;
 };
 
 class AFPCMDigitalAudioInterface
 {
 public:
-    typedef boost::function<void ()> send_audio_sync_info_fct_t;
-
     AFPCMDigitalAudioInterface(boost::shared_ptr<OpenAudioOutputReq> req);
     ~AFPCMDigitalAudioInterface();
 
-    void setSendAudioSyncInfo(send_audio_sync_info_fct_t fct);
-
     bool play(boost::shared_ptr<AFAudioFrame> frame);
     bool getOverallLatency(snd_pcm_sframes_t& delay);
-    snd_pcm_sframes_t getBufferFillLevel();
-    double getNextPTS();
-
-    void start();
-    bool pause(bool enable);
-    void stop();
-
-    void skip(boost::shared_ptr<AFAudioFrame> frame, double seconds);
 
 private:
     AFPCMDigitalAudioInterface();
@@ -149,21 +103,19 @@ private:
     snd_pcm_format_t format;
     unsigned int bytesPerSample;
 
-    snd_pcm_uframes_t buffer_size;
+    snd_pcm_sframes_t buffer_size;
     snd_pcm_uframes_t period_size;
     unsigned int buffer_time;        // ring buffer length in us
     unsigned int period_time;        // period time in us
-
-    send_audio_sync_info_fct_t sendAudioSyncInfo;
-    double nextPTS;  // PTS of the next frame written to playback buffer
-
+    
     int xrun_recovery(int err);
+
+    bool first;
+
     bool directWrite(boost::shared_ptr<AFAudioFrame> frame);
     bool copyFrame(const snd_pcm_channel_area_t *areas,
 		   snd_pcm_uframes_t offset,
 		   int frames, boost::shared_ptr<AFAudioFrame> frame);
-
-    boost::shared_ptr<CopyLog> m_CopyLog;
 };
 
 #endif
