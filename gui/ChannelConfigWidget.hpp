ebf4e9f1a86b7fb58aa0a6b94c0ac42bfcb15c2d
Joachim Erbs <joachim.erbs@gmx.de>
2010-07-31 17:41:35 +0200

    Adding GPLv3 headers

--- gui/ChannelConfigWidget.hpp
+++ gui/ChannelConfigWidget.hpp
@@ -3,6 +3,21 @@
 //
 // Copyright (C) Joachim Erbs, 2010
 //
+//    This file is part of Sinema.
+//
+//    Sinema is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Sinema is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Sinema.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef CHANNEL_CONFIG_WIDGET_HPP
 #define CHANNEL_CONFIG_WIDGET_HPP
