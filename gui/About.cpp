7255b728f7db5bfe6b191259a6e97768f6eea14d
Joachim Erbs <joachim.erbs@gmx.de>
2010-08-01 23:10:00 +0200

    README
    Text in the help dialog is generated from README.

--- gui/About.cpp
+++ gui/About.cpp
@@ -22,6 +22,7 @@
 #include "gui/About.hpp"
 #include "gui/General.hpp"
 #include "gui/COPYING.inc"
+#include "gui/README.inc"
 #include "gui/version.inc"
 // #include "gui/logo.xpm"
 
@@ -31,8 +32,8 @@ const std::string applicationName("sinema");
 
 AboutDialog::AboutDialog()
 {
-    std::string copying(&COPYING[0], sizeof(COPYING));
-    std::string version(&::version[0], sizeof(::version));
+    std::string copying(COPYING, sizeof(COPYING));
+    std::string version(::version, sizeof(::version));
     set_program_name(applicationName);
     set_version(version);
     set_comments("A Media Player and TV Viewer");
@@ -59,97 +60,106 @@ void AboutDialog::on_response (int)
 
 HelpDialog::HelpDialog()
 {
+    using std::string;
+
     set_title(applicationName + " (Help)");
     set_default_size(520,1500);
+
+    string readme(README, sizeof(README));
 
     m_ScrolledWindow.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
 
     m_refTextBuffer = Gtk::TextBuffer::create();
 
+    // line starts with 2 or more spaces
     TextTagPtr tagHeading1 = m_refTextBuffer->create_tag("heading1"); 
     tagHeading1->property_weight() = PANGO_WEIGHT_BOLD;
-    tagHeading1->property_size() = 20 * Pango::SCALE;
+    tagHeading1->property_justification() = Gtk::JUSTIFY_CENTER;
+    tagHeading1->property_size() = 18 * Pango::SCALE;
 
+    // line starts with 2 or more spaces and ends with a tab
     TextTagPtr tagHeading2 = m_refTextBuffer->create_tag("heading2"); 
     tagHeading2->property_weight() = PANGO_WEIGHT_BOLD;
-    tagHeading2->property_size() = 15 * Pango::SCALE;
+    tagHeading2->property_justification() = Gtk::JUSTIFY_CENTER;
+    tagHeading2->property_size() = 14 * Pango::SCALE;
+
+    // line ends with a tab
+    TextTagPtr tagHeading3 = m_refTextBuffer->create_tag("heading3"); 
+    tagHeading3->property_weight() = PANGO_WEIGHT_BOLD;
+    tagHeading3->property_size() = 14 * Pango::SCALE;
 
     TextTagPtr tagBody = m_refTextBuffer->create_tag("body"); 
-    tagBody->property_font() = "Courier";
     tagBody->property_size() = 10 * Pango::SCALE;
     tagBody->property_wrap_mode() = Gtk::WrapMode::WRAP_WORD;
 
+    // line contains 2 subsequent spaces
     TextTagPtr tagNoWrap = m_refTextBuffer->create_tag("nowrap"); 
     tagNoWrap->property_font() = "Courier";
     tagNoWrap->property_size() = 10 * Pango::SCALE;
     tagNoWrap->property_wrap_mode() = Gtk::WrapMode::WRAP_NONE;
 
-
-    std::string description = applicationName + " is a free, GPL licensed "
-	"media player and TV application.\n"
-	"\n"
-	"FFmpeg is used for video and audio decoding. " + applicationName + " "
-	"can play every format supported by the installed FFmpeg library.\n"
-	"\n"
-	"ALSA is used to access the sound card. Accessing ALSA directly is "
-	"possible from a single application only. Usually this is not wanted. "
-	"It can be avoided by using Pulse Audio with a virtual ALSA device. "
-	"Using Pulse Audio directly is not supported.\n"
-	"\n"
-	"XVideo is the only supported video output interface.\n"
-	"\n"
-	"TV viewing is only supported with analog TV cards having an "
-	"MPEG hardware encoder. It is expected, that the encoded MPEG data is "
-	"available at /dev/video0. The command \"cp /dev/video0 tv.mpg\" should "
-	"produce a valid MPEG file. Otherwise watching TV will not be possible "
-	"with " + applicationName + ". Watching TV is tested with the "
-	"IVTV driver. See <http://ivtvdriver.org> for details about supported "
-	"TV cards.\n"
-	"\n"
-	"The time shifting implementation is still very basic. Live TV is "
-	"written to /tmp/tv.mpg and simultaneously that file is played. The "
-	"file name is hard-coded. During a TV session that file "
-	"isn\'t truncated. Ensure that the partition at /tmp provides enough "
-	"free space. \n"
-	"\n"
-	"Cropping the video picture is possible to avoid black borders. Some "
-	"cropping areas are predefined and can be enabled via the View menu. "
-	"Alternatively the cropping area can intuitively be selected using the "
-	"mouse. Move the mouse pointer to the edges and press the right mouse "
-	"button. The shape of the mouse pointer indicates the action. Pressing "
-	"the right mouse button, when the mouse is placed in the middle of the "
-	"video picture, undoes any cropping.\n"
-	"\n"
-	"Pressing the middle mouse button shows and hides an additional control "
-	"window. Pressing the right mouse button opens a context menu with the "
-	"most important items also available in the main menu. This is especially "
-	"useful for the full screen mode.\n" 	
-	"\n";
-
-    std::string hotkeyDescr = 
-	"Cursor-Left                  Jump 10 seconds back\n"
-	"Cursor-Right                 Jump 10 seconds forward\n"
-	"Shift + Cursor-Left          Jump 30 seconds back\n"
-	"Shift + Cursor-Right         Jump 30 seconds forward\n"
-	"Ctrl + Cursor-Left           Jump 60 seconds back\n"
-	"Ctrl + Cursor-Right          Jump 60 seconds forward\n"
-	"Ctrl + Shift + Cursor-Left   Jump 180 seconds back\n"
-	"Ctrl + Shift + Cursor-Right  Jump 180 seconds forward\n"
-	"Space                        Toggle play/pause\n"
-	"Page Up                      Skip to previous play list entry\n"
-	"Page Down                    Skip to next play list entry\n"
-	"Cursor-Up                    Increase volume\n"
-	"Cursor-Down                  Decrease volume\n"
-	"m                            Toggle mute mode\n"
-	"Escape                       Leave full screen mode\n";
-
     Gtk::TextBuffer::iterator it = m_refTextBuffer->end();
 
-    it = m_refTextBuffer->insert_with_tag(it, applicationName + "\n\n", tagHeading2);
-    it = m_refTextBuffer->insert_with_tag(it, description, tagBody);
+    string::size_type sol = 0;
+    string paragraph;
 
-    it = m_refTextBuffer->insert_with_tag(it, "Hotkeys\n\n", tagHeading2);
-    it = m_refTextBuffer->insert_with_tag(it, hotkeyDescr, tagNoWrap);
+    while(sol < readme.size())
+    {
+	string::size_type eol = readme.find_first_of('\n', sol);
+	if (eol == string::npos) eol = readme.size();
+
+	string line(readme, sol, eol-sol);
+
+	if (line.empty())
+	{
+	    if (!paragraph.empty())
+	    {
+		paragraph += "\n";
+		it = m_refTextBuffer->insert_with_tag(it, paragraph, tagBody);
+		paragraph.clear();
+	    }
+
+	    it = m_refTextBuffer->insert_with_tag(it, "\n", tagBody);
+	}
+	else
+	{
+	    string::size_type posDoubleSpace = line.find("  ");
+
+	    bool tab = (line[line.size()-1] == '\t');
+	    if (tab) line.erase(line.size()-1);
+
+	    if (posDoubleSpace == 0)
+	    {
+		line.erase(0, line.find_first_not_of(' '));
+		line += "\n";
+		if (!tab)
+		{
+		    it = m_refTextBuffer->insert_with_tag(it, line, tagHeading1);
+		}
+		else
+		{
+		    it = m_refTextBuffer->insert_with_tag(it, line, tagHeading2);
+		}
+	    }
+	    else if (tab)
+	    {
+		line += "\n";
+		it = m_refTextBuffer->insert_with_tag(it, line, tagHeading3);
+	    }
+	    else if (posDoubleSpace != string::npos)
+	    {
+		line += "\n";
+		it = m_refTextBuffer->insert_with_tag(it, line, tagNoWrap);
+	    }
+	    else
+	    {
+		if (!paragraph.empty()) paragraph += " ";
+		paragraph += line;
+	    }
+	}
+
+	sol = eol + 1;
+    }
 
     m_TextView.set_buffer(m_refTextBuffer);
     m_TextView.set_editable(false);
@@ -171,4 +181,3 @@ void HelpDialog::on_response (int)
 }
 
 // ===================================================================
-
